024886f8e60344dde6e67d3031bb47effadddcc7
Charles McLeod <charlesmcleod@usf.edu>
2025-01-25 19:13:36 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+// Task 1:
+let employeeName = "John Smith";
+const employeeID = 64610334;
+var isActive = true;
+console.log("employeeName: " + employeeName + ", Type: " + typeof employeeName);
+console.log("employeeID: " + employeeID + ", Type: " + typeof employeeID);
+console.log("isActive: " + isActive + ", Type: " + typeof isActive);
