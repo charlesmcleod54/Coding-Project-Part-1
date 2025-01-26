aa327682982beccfcd33d1a70925e31c88d1a317
Charles McLeod <charlesmcleod@usf.edu>
2025-01-25 19:22:56 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,10 @@ var isActive = true;
 console.log("employeeName: " + employeeName + ", Type: " + typeof employeeName);
 console.log("employeeID: " + employeeID + ", Type: " + typeof employeeID);
 console.log("isActive: " + isActive + ", Type: " + typeof isActive);
+// Task 2:
+let productName = "Football Helmets";
+const productPrice = 199.99;
+var isAvailable = true;
+console.log("productName:", productName, "| Type:", typeof productName);
+console.log("productPrice:", productPrice, "| Type:", typeof productPrice);
+console.log("isAvailable:", isAvailable, "| Type:", typeof isAvailable);
